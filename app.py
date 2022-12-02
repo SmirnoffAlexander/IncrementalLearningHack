042a5dc88e3ae348901c63f27c383fa3a10bb02f
rw404 <trapeznikovmisha@mail.ru>
2022-12-02 21:57:25 +0300

    bad demo v1

--- app.py
+++ app.py
@@ -12,8 +12,11 @@ def main():
     # If a form is submitted
     if request.method == "POST":
         
+        if request.form.get('action1') == 'HOME':
+            return render_template("index.html")
         # Get values through input bars
         print("@@@@@@@@@@@@@@@@@@@")
+        print(id)
         print(request.form.get("file")) 
         # Put inputs to dataframe
         #X = pd.DataFrame([[height, weight]], columns = ["Height", "Weight"])
@@ -21,10 +24,9 @@ def main():
         # Get prediction
         #prediction = clf.predict(X)[0]
         prediction = "AAAA"
+        return render_template("generic.html", output=prediction)
     else:
-        prediction = ""
-        
-    return render_template("index.html", output = prediction)
+        return render_template("index.html")
 
 # Running the app
 if __name__ == '__main__':
