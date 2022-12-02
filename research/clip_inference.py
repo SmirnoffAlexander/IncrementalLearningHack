571c800087416584251c94c0a6b3341358d62d02
Alexander Smirnov <alexsmirnov0101@mail.ru>
2022-12-03 02:23:37 +0300

    some updates

--- research/clip_inference.py
+++ research/clip_inference.py
@@ -1,6 +1,9 @@
 import json
+import os
 
 import clip
+import numpy as np
+import plotly.graph_objects as go
 import torch
 
 
@@ -23,7 +26,7 @@ class ClipModel:
             logits_per_image, _ = self.model(image, text)
             probs = logits_per_image.softmax(dim=-1).cpu().numpy().ravel()
             res_class = self.class_names[probs.argmax()]
-        return res_class
+        return res_class, probs
 
 
 if __name__ == "__main__":
@@ -32,10 +35,18 @@ if __name__ == "__main__":
         image_net_names = list(json.load(f).values())
     main_classes = ["a tractor", "a lawnmower", "a bicycle", "a snowboard",
                     "ski", "a truck", "a minibus", "a train", "a dump truck", "a horse"]
-
-    img = Image.open('Classes10/lawnmower2.jpg')
-
     all_classes = main_classes + image_net_names
-    print(len(all_classes))
-    model = ClipModel(class_names=all_classes)
-    print(model(img))
+    for cl in os.listdir('images'):
+        os.makedirs(os.path.join('plots', cl), exist_ok=True)
+        for img_name in os.listdir(os.path.join('images', cl)):
+            plot_img_name = os.path.join('plots', cl, "plot_"+img_name)
+            full_img_name = os.path.join('images', cl, img_name)
+            img = Image.open(full_img_name)
+
+            model = ClipModel(class_names=main_classes)
+            res_class, probs = model(img)
+            class_inds = probs.argsort()[::-1][:5]
+            pred_probs = probs[class_inds]
+            pred_classes = np.array(main_classes)[class_inds].tolist()
+            fig = go.Figure([go.Bar(x=pred_classes, y=pred_probs)])
+            fig.write_image(plot_img_name)
