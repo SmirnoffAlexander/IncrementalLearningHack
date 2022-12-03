4e32e72c48902f75c06a796434e213ac2ed00b9d
rw404 <trapeznikovmisha@mail.ru>
2022-12-03 11:18:34 +0300

    CheckPoint 1 demo

--- app.py
+++ app.py
@@ -1,32 +1,41 @@
 from flask import Flask, request, render_template
 import pandas as pd
-import joblib
-
+from werkzeug.utils import secure_filename
+import os
+import shutil
+from clippper import clipInference
 
 # Declare a Flask app
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'Inference'
+app.config['MAX_CONTENT_PATH'] = 5000
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    
     # If a form is submitted
     if request.method == "POST":
         
-        if request.form.get('action1') == 'HOME':
+        if request.form.get('action') == 'welcome':
             return render_template("index.html")
+        elif request.form.get('action') == 'inference' or request.form.get('action') == "try it!":
+            shutil.copyfile('images/pic01.jpg', 'static/images/pic01.jpg')
+            return render_template("inference.html")
         # Get values through input bars
         print("@@@@@@@@@@@@@@@@@@@")
-        print(id)
-        print(request.form.get("file")) 
+        file = request.files['file']
+        fname = 'infer.jpg'
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], fname))
+        shutil.copyfile('Inference/infer.jpg', 'static/images/pic01.jpg')
         # Put inputs to dataframe
         #X = pd.DataFrame([[height, weight]], columns = ["Height", "Weight"])
         
         # Get prediction
         #prediction = clf.predict(X)[0]
-        prediction = "AAAA"
-        return render_template("generic.html", output=prediction)
+        prediction = f'Predicted class is {clipInference()}.'
+        return render_template("inference.html", output=prediction)
     else:
-        return render_template("index.html")
+        pass 
+    return render_template("index.html")
 
 # Running the app
 if __name__ == '__main__':
